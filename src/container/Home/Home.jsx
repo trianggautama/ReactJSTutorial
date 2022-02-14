5ace9141b79b1201685e482fc238dc9b2c1e3db0
trianggautama <trianggategarutama@gmail.com>
2022-02-14 16:16:49 +0800

    react routing

--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -1,9 +1,14 @@
-import React, {Component} from "react";
+import React, {Component,Fragment} from "react";
 import BlogPost from "../BlogPost/BlogPost";
 // import YoutubeComponent from "../../component/YoutubeComponent/YoutubeComponent";
-// import LifeCycle from "../LifeCycle/LifeCycle";
-// import Product from "../Product/Product";
-
+import LifeCycle from "../LifeCycle/LifeCycle";
+import Product from "../Product/Product";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Link
+  } from "react-router-dom";
 class Home extends Component {
     state = {
         showComponent: true
@@ -19,28 +24,21 @@ class Home extends Component {
 
     render(){
        return(
-        <div>
-            {/* <p>Youtube</p>
-            <hr />
-            <YoutubeComponent time="04.00" title="Judul Vidio 1" desc="lorem ipsum bla bla bla bla ...."/>
-            <YoutubeComponent time="09.10" desc="lorem ipsum bla bla bla bla ...." />
-            <YoutubeComponent title="Judul Vidio 3" desc="lorem ipsum bla bla bla bla ...."/>
-            <YoutubeComponent time="04.00" title="Judul Vidio 4" desc="lorem ipsum bla bla bla bla ...."/>
-            <YoutubeComponent time="10.00" title="Judul Vidio 5" desc="lorem ipsum bla bla bla bla ...."/> */}
-            {/* <p>Counter</p>
-            <hr />
-            <Product/> */}
-            {/* <p>LifeCycle</p>
-            <hr />
-            {
-                this.state.showComponent ?             
-                <LifeCycle/>
-                : null
-            } */}
-            <p>Blog Post</p>
-            <hr />
-            <BlogPost/>
-        </div>
+        <BrowserRouter>
+           <Fragment>
+            <h1>Menu</h1>
+            <nav>
+                <Link to="/">Home</Link> 
+                <Link to="/product">Product</Link>
+                <Link to="/lifecycle">Lifecycle</Link>
+            </nav>
+           <Routes>
+                 <Route path="/" element={<BlogPost />}></Route>
+                 <Route path="/product" element={<Product/>}></Route>
+                 <Route path="/lifecycle" element={<LifeCycle/>}></Route>
+            </Routes>
+           </Fragment>
+        </BrowserRouter>
        )
     }
 }
